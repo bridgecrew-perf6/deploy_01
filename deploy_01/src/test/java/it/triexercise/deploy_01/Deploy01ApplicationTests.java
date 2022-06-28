3265e2af45090a244407b2e14c0bdf7ee871ca94
Tristan Geronimo <tristanpgeronimo@gmail.com>
2022-06-28 10:17:33 +0200

    Add files via upload

---
+++ deploy_01/src/test/java/it/triexercise/deploy_01/Deploy01ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.triexercise.deploy_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
